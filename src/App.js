324391cd703d4335775a93c77a2a4732515d0ed2
Micha-els <chigbumichaelnd@gmail.com>
2021-12-01 02:07:15 +0100

    loading page icon rectified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from './NavBar';
 import CharactersPage from './CharactersPage';
 import Home from './Home';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import NotFound from './NotFound';
+// import NotFound from './NotFound';
 
 function App() {
   return (
--- src/LoadPage.js
+++ src/LoadPage.js
@@ -3,10 +3,10 @@ import { Image } from 'react-bootstrap'
 
 const LoadPage = () => {
     return (
-        <div className="d-flex justify-content-center align-items-center vh-100">
-            <Image src='../assets/planet-loader.svg'/>
+        <div className="d-flex justify-content-center align-items-center vh-100">   
+            <Image src={process.env.PUBLIC_URL+'/assets/planet-loader.svg'}/>
         </div>
     )
 }
 
-export default LoadPage
+export default LoadPage;
